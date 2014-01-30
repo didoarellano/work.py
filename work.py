2d1228438b83bad60b1e09b65c1594791db6547e
Dido Arellano <dido@semicolonstudios.com>
2014-01-30 18:44:36 +0800

    Create checkpoint branch & commit on `work end .`

--- work.py
+++ work.py
@@ -19,6 +19,9 @@ def main():
         print('work needs to be called in a git-tracked project\'s root directory')
         return
 
+    if args.action == 'end' and args.repository != 'all':
+        save_repository_state(cwd)
+
 
 def in_git_toplevel(dir):
     try:
@@ -29,6 +32,13 @@ def in_git_toplevel(dir):
         return toplevel.strip() == dir
     except:
         return False
+
+
+def save_repository_state(dir):
+    commit_message = 'Work end checkpoint commit'
+    subprocess.call(['git', 'checkout', '-B', 'work-end-checkpoint'], cwd=dir)
+    subprocess.call(['git', 'add', '.'], cwd=dir)
+    subprocess.call(['git', 'commit', '--message', commit_message], cwd=dir)
 
 
 if __name__ == '__main__':
