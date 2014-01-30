8600db2c0fadc869aebe0b4674b0cc03778a4ea2
Dido Arellano <dido@semicolonstudios.com>
2014-01-30 17:50:28 +0800

    Make sure work is called in git toplevel dir

--- work.py
+++ work.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
+import os
 import argparse
+import subprocess
 
 
 def main():
@@ -11,6 +13,22 @@ def main():
                         default='all',
                         help='default is "all"')
     args = parser.parse_args()
+
+    cwd = os.getcwd()
+    if not in_git_toplevel(cwd):
+        print('work needs to be called in a git-tracked project\'s root directory')
+        return
+
+
+def in_git_toplevel(dir):
+    try:
+        toplevel = subprocess.check_output(
+            ['git', 'rev-parse', '--show-toplevel'],
+            cwd=dir
+        )
+        return toplevel.strip() == dir
+    except:
+        return False
 
 
 if __name__ == '__main__':
